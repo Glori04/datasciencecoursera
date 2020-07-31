a8c147f2964d86f281b7bb4c4682490d548ca0b3
Gloriana <62759368+Glori04@users.noreply.github.com>
2020-07-31 09:25:50 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,16 @@
 
 ## Write a short comment describing this function
 
+## **The function below creates an R object and stores a vector**##
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
+
+## **The function below retrieve the value that is stored in the Vector**##
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
